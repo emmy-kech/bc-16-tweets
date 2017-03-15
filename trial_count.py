a77e02d06904c0b23496e6f974c5e48a1b4ba28c
The Kech's <The Kech's>
2017-03-15 16:07:22 +0300

    This shows the word count but with stop words

---
+++ trial_count.py
@@ -0,0 +1,47 @@
+from twython import Twython, TwythonError
+import time
+import json
+from tqdm import tqdm
+from collections import Counter
+
+api_key="XEJB5Rf0AaoLNo4oAsK7oWaG5"
+api_secret="dm9BR5nvV74e74wqgzVezDVEm8iDbfZyXtUQ2UdEcH4Gq3dNt7"
+access_key="780742606418358272-dbpVU5L9uzyhXk4TTiD5z2nylTq7yiz"
+access_secret ="ktQWfh066fJZY1qX0cjansLM5NKjvlzz9f8a1aMVjMbLz"
+
+twitter = Twython(api_key,api_secret, access_key,access_secret)
+
+
+def user_id(username = ''):
+	username = input("\nEnter user's Twitter handle: ")
+	return username	
+
+def fetch_tweets(username=''):
+	user_tweets= twitter.get_user_timeline(screen_name = username, count = 50) 
+	tweet_text = []
+	output=''																		
+	bar=tqdm(total=int(len(user_tweets)))
+	
+	for tweets in user_tweets:
+
+		# for i in tqdm (range(int(len(user_tweets)))):
+		time.sleep(0.1)	
+		tweet_text.append(tweets['text'])
+		bar.update(1)
+	bar.close()
+		# return(tweet_text)
+	print(len(tweet_text))
+	for results in tweet_text:
+		output+=(results)
+	word_list=output.split(' ')
+	c=Counter(word_list)
+	return c.most_common(5)
+
+
+	with open('tweets.json', 'w') as f:
+		json.dump(tweet_text,f)
+	return(output)
+	
+    
+print(fetch_tweets('realDonaldTrump'))
+
