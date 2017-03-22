7df7076d9484bf4cdd0ec900dcf57b2f3cbae92e
emmy-kech <emmykech2@gmail.com>
2017-03-22 11:52:23 +0300

    Delete settings.py

--- settings.py
+++
@@ -1,5 +0,0 @@
-# Twitter Oauth credentials
-api_key="XEJB5Rf0AaoLNo4oAsK7oWaG5"
-api_secret="dm9BR5nvV74e74wqgzVezDVEm8iDbfZyXtUQ2UdEcH4Gq3dNt7"
-access_key="780742606418358272-dbpVU5L9uzyhXk4TTiD5z2nylTq7yiz"
-access_secret ="ktQWfh066fJZY1qX0cjansLM5NKjvlzz9f8a1aMVjMbLz"
